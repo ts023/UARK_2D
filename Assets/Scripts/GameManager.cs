0da93430781f2080ef3e1380ecc1aa7c655e98dd
ts023 <31971139+ts023@users.noreply.github.com>
2017-09-29 09:52:36 -0500

    GameManager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,13 +4,23 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
 
+		public BoardManager boardScript;
+
+		private int level = 3;
+
 	// Use this for initialization
-	void Start () {
-		
+	void Awake ()
+	{
+			boardScript = GetComponent<BoardManager>();
+			InitGame();
 	}
-	
+
+	void InitGame()
+	{
+			boardScript.SetupScene(level);
+	}
 	// Update is called once per frame
 	void Update () {
-		
+
 	}
 }
